b4e48acb1982236d91e09945bf266e69426015d7
Brian Mask <brian@brianmask.com>
2020-01-31 22:26:53 -0700

    boiler additions

--- fasthomeapi/migrations/env.py
+++ fasthomeapi/migrations/env.py
@@ -17,6 +17,10 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
+from app.settings import DATABASE_URL
+
+config.set_main_option('sqlalchemy.url', str(DATABASE_URL))
 target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
